38b149421f6196697073e7960b51cb06b3aa96cf
VICTOR <155731790+spirit0621@users.noreply.github.com>
2025-11-23 17:05:40 +0100

    Delete src directory

--- src/main/java/cinemax/repository/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package cinemax.repository;
-
-import cinemax.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
