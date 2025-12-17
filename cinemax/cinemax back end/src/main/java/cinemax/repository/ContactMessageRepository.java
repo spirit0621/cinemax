4fdfa2a80c12cabba52cdd128337b46858d3806c
VICTOR <155731790+spirit0621@users.noreply.github.com>
2025-12-17 15:00:58 +0100

    Delete cinemax directory

--- cinemax/cinemax back end/src/main/java/cinemax/repository/ContactMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package cinemax.repository;
-
-import cinemax.model.ContactMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
-}
